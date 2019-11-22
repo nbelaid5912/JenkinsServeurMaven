f6ffff993764585174bbc7891f6ebf5c305cfd25
usager <usager@10.0.2.15>
2019-11-22 15:48:48 -0500

    version 1.1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author usager
+ */
+public class Main {
+    public static void main(int[] args){
+        int result = add(args[0],args[1]);
+    }
+    public static int add(int x, int y){
+        return x + y ;
+    }
+        
+}
