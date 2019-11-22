df55db8812e83add9252134923bdf358a28a3261
usager <usager@10.0.2.15>
2019-11-22 16:06:43 -0500

    version 1.3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,11 +10,6 @@ package main;
  * @author usager
  */
 public class Main {
-    public static void main(int[] args){
-        args[0] = 5;
-        args[1] = 5;
-        int result = add(args[0],args[1]);
-    }
     public static int add(int x, int y){
         return x + y ;
     }
